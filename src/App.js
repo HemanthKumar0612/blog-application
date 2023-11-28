db01d26ec442d4d5aad41742d4ca6f9c36bfcc57
Hemanth Kumar <hemanthkumarmulka15@gmail.com>
2023-11-28 16:14:03 +0530

    Added AddBlog Feature Working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import React,{ useState } from 'react';
 import './App.css';
 import AddBlog from './components/AddBlog';
 
 function App() {
+
+  // Creating State variable blogs[]
+  const [blogs,setBlogs] = useState([]);
+
+  // Add Blog Handler
+  const handleAddBlog = (newBlog) => {
+
+    // console.log("Inside handleAddBlog");
+    // console.log(newBlog);
+    // Shorthand for adding blogs
+    setBlogs((prevBlogs) => [...prevBlogs, newBlog]);
+    console.log(blogs);
+  }
+
   return (
     <div className="App">
-      <AddBlog></AddBlog>
+      {/* Passing handleAddBlog function as a prop to AddBlog FC */}
+      <AddBlog handleAddBlog = {handleAddBlog}></AddBlog>
     </div>
+
   );
 }
 
--- src/components/AddBlog.jsx
+++ src/components/AddBlog.jsx
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const AddBlog = () => {
+
+const AddBlog = ({handleAddBlog}) => {
+
+    // Creating State Variable for New Blog
+    const [blogData, setBlogData] = useState({
+        title: '',
+        body: '',
+    });
+    
+
+    // This takes a variable from input field and adds it as a key-value pair in blogData
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setBlogData({ ...blogData, [name]: value });
+        // console.log("Inside handleChange ");
+        // console.log(e);
+    }
+
+    // On submit call onNewBlog function from parent component
+    const handleSubmit = (e) => {
+
+        // preventing Default submission action by web browser
+        e.preventDefault();
+        // console.log("Inside handleSubmit ");
+        // console.log(blogData);
+        handleAddBlog(blogData);
+    }
+    
+
     return (
         <div className='container-fluid'>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h1>Compose</h1>
                 <div class="mb-3 form-group">
                     <label className="form-label" htmlFor="title">Title</label>
-                    <input className="form-control" type="text" name="title" id="title" />
+                    <input className="form-control" type="text" name="title" id="title" value={blogData.title} onChange={handleChange}/>
                 </div>
                 <div class="mb-3 form-group">
                     <label className="form-label" htmlFor="body">Post</label>
-                    <textarea className="form-control" name="body" id="body" rows="5"></textarea>
+                    <textarea className="form-control" name="body" id="body" rows="5" value={blogData.body} onChange={handleChange}></textarea>
                 </div>
                 <div className="mb-3 form-group">
                     <button className="btn btn-primary" type="submit">Publish</button>
